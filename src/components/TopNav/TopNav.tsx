765b3814c5eb85f98158a6e4f0de5d5c7f59b4fe
Shannice <63969928+Shannice@users.noreply.github.com>
2024-07-09 01:07:54 +0100

    Update TopNav.tsx

--- src/components/TopNav/TopNav.tsx
+++ src/components/TopNav/TopNav.tsx
@@ -1,18 +1,17 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
-import React, { Suspense, lazy, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import TopNavigation from '@cloudscape-design/components/top-navigation';
 import { TopNavigationProps } from '@cloudscape-design/components/top-navigation';
 import { Density, Mode, applyDensity, applyMode } from '@cloudscape-design/global-styles';
 
-import ModalLoader from '@/components/SuspenseLoader/ModalLoader';
+import Auth from '@/components/Auth';
 import { useAppThemeContext } from '@/store/appTheme';
 import { useAuthContext } from '@/store/auth';
+import { isUserEmailVerified } from '@/utils/Auth/isUserEmailVerified';
 
 import './TopNav.css';
-
-const Auth = lazy(() => import('@/components/Auth'));
 
 type TopNavClick = {
     detail: {
@@ -21,55 +20,56 @@ type TopNavClick = {
 };
 
 export default function TopNav() {
-    const { isUserAuthenticated, user, signOut } = useAuthContext();
-    const { appTheme, setAppThemeColor, setAppThemeDensity } = useAppThemeContext();
+    const { user, signOut } = useAuthContext();
+    const { appTheme, setAppTheme } = useAppThemeContext();
 
     const [authVisible, setAuthVisible] = useState(false); // authentication modal visibility
+    const [density, setDensity] = useState('density.comfortable'); // app density
 
-    // Set app appTheme
+    // Set appTheme
     useEffect(() => {
-        if (appTheme.color === 'appTheme.light') {
-            applyMode(Mode.Light);
-        } else if (appTheme.color === 'appTheme.dark') {
-            applyMode(Mode.Dark);
-        }
-
-        if (appTheme.density === 'density.comfortable') {
-            applyDensity(Density.Comfortable);
-        } else if (appTheme.density === 'density.compact') {
-            applyDensity(Density.Compact);
+        switch (appTheme) {
+            case 'theme.light':
+                applyMode(Mode.Light);
+                break;
+            case 'theme.dark':
+                applyMode(Mode.Dark);
+                break;
+            default:
+                break;
         }
     }, [appTheme]);
 
     // When user authenticates, close authentication modal window
     useEffect(() => {
-        if (isUserAuthenticated) {
+        if (isUserEmailVerified(user)) {
             setAuthVisible(false);
         }
-        // no else because we want the appAuth window to only pop up by clicking sign in, not automatically
-    }, [isUserAuthenticated]);
+        // no else because we want the auth window to only pop up by clicking sign in, not automatically
+    }, [user]);
 
     // Change visualization
     function handleUtilVisualClick(e: TopNavClick) {
         switch (e.detail.id) {
-            case 'appTheme.light':
-                setAppThemeColor('appTheme.light');
+            case 'theme.light':
+                setAppTheme('theme.light');
                 break;
-            case 'appTheme.dark':
-                setAppThemeColor('appTheme.dark');
+            case 'theme.dark':
+                setAppTheme('theme.dark');
                 break;
             case 'density.comfortable':
-                setAppThemeDensity('density.comfortable');
+                applyDensity(Density.Comfortable);
+                setDensity('density.comfortable');
                 break;
             case 'density.compact':
-                setAppThemeDensity('density.compact');
+                applyDensity(Density.Compact);
+                setDensity('density.compact');
                 break;
             default:
                 break;
         }
     }
 
-    // App appTheme dropdown
     const utilVisual: TopNavigationProps.MenuDropdownUtility = {
         type: 'menu-dropdown',
         iconName: 'settings',
@@ -77,19 +77,19 @@ export default function TopNav() {
         title: 'Settings',
         items: [
             {
-                id: 'appTheme',
+                id: 'theme',
                 text: 'Theme',
                 items: [
                     {
-                        id: 'appTheme.light',
+                        id: 'theme.light',
                         text: 'Light',
-                        disabled: appTheme.color === 'appTheme.light',
+                        disabled: appTheme === 'theme.light',
                         disabledReason: 'Currently selected',
                     },
                     {
-                        id: 'appTheme.dark',
+                        id: 'theme.dark',
                         text: 'Dark',
-                        disabled: appTheme.color === 'appTheme.dark',
+                        disabled: appTheme === 'theme.dark',
                         disabledReason: 'Currently selected',
                     },
                 ],
@@ -101,13 +101,13 @@ export default function TopNav() {
                     {
                         id: 'density.comfortable',
                         text: 'Comfortable',
-                        disabled: appTheme.density === 'density.comfortable',
+                        disabled: density === 'density.comfortable',
                         disabledReason: 'Currently selected',
                     },
                     {
                         id: 'density.compact',
                         text: 'Compact',
-                        disabled: appTheme.density === 'density.compact',
+                        disabled: density === 'density.compact',
                         disabledReason: 'Currently selected',
                     },
                 ],
@@ -116,35 +116,35 @@ export default function TopNav() {
         onItemClick: (e) => handleUtilVisualClick(e),
     };
 
-    // User appAuth dropdown (if appAuth) else sign-in
-    const utilUser: TopNavigationProps.ButtonUtility | TopNavigationProps.MenuDropdownUtility = isUserAuthenticated
-        ? {
-              type: 'menu-dropdown',
-              text: user?.signInDetails?.loginId || user?.username,
-              description: user?.signInDetails?.loginId,
-              iconName: 'user-profile',
-              items: [{ id: 'signout', text: 'Sign out' }],
-              onItemClick: () => signOut(),
-          }
-        : {
-              type: 'button',
-              text: 'Sign In',
-              onClick: () => setAuthVisible(true),
-          };
+    const utilUser: TopNavigationProps.ButtonUtility | TopNavigationProps.MenuDropdownUtility =
+        user && isUserEmailVerified(user)
+            ? {
+                  type: 'menu-dropdown',
+                  text: user?.attributes?.email || user?.username,
+                  description: user?.attributes?.email,
+                  iconName: 'user-profile',
+                  items: [{ id: 'signout', text: 'Sign out' }],
+                  onItemClick: () => signOut(),
+              }
+            : {
+                  type: 'button',
+                  text: 'Sign In',
+                  onClick: () => setAuthVisible(true),
+              };
 
     const navUtils = [utilVisual, utilUser];
 
     return (
         <>
-            {authVisible && (
-                <Suspense fallback={<ModalLoader />}>
-                    <Auth setVisible={setAuthVisible} />
-                </Suspense>
-            )}
+            <Auth visible={authVisible} setVisible={setAuthVisible} />
             <TopNavigation
                 identity={{
                     href: '/',
-                    title: 'Amazon Web Services',
+                    title: 'AWS Conversación Agente',
+                    logo: {          
+                        src: "/logo.png",          
+                        alt: "Service"        
+                    }
                 }}
                 utilities={navUtils}
             />
